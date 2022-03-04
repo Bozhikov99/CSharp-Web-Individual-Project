dc00258edd20f9d2d9c222848aa787b489932b96
Bozhikov99 <72099913+Bozhikov99@users.noreply.github.com>
2022-03-04 10:53:31 +0200

    Added an ASP.NET MVC Web project. Added Repositories to the IoC container. Developing Services.

---
+++ CinemaIgnite/Core/Services/Contracts/IGenreService.cs
@@ -0,0 +1,15 @@
+﻿using Core.ViewModels.Genre;
+
+namespace Core.Services.Contracts
+{
+    public interface IGenreService
+    {
+        Task<bool> Create(CreateGenreModel model);
+
+        Task<bool> Edit();
+
+        Task<ListGenreModel> GetById(string id);
+
+        Task<ICollection<ListGenreModel>> GetAll();
+    }
+}
---
+++ CinemaIgnite/Core/ViewModels/Genre/CreateGenreModel.cs
@@ -0,0 +1,11 @@
+﻿using Common;
+using System.ComponentModel.DataAnnotations;
+
+namespace Core.ViewModels.Genre
+{
+    public class CreateGenreModel
+    {
+        [StringLength(GenreConstants.NameMaxLength, MinimumLength = GenreConstants.NameMinLength, ErrorMessage = "{0} must be between {2} and {1} characters long.")]
+        public string Name { get; set; }
+    }
+}
