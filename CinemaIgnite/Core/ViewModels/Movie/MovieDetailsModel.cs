70e23e31c1d82edac3b71a595d528a08d86d78b5
Bozhikov99 <72099913+Bozhikov99@users.noreply.github.com>
2022-03-13 10:17:55 +0200

    Wrote the MovieDetailsModel for the Details.cshtml page of each movie. About to scaffold Identity.

--- CinemaIgnite/Core/ViewModels/Movie/MovieDetailsModel.cs
+++ CinemaIgnite/Core/ViewModels/Movie/MovieDetailsModel.cs
@@ -8,5 +8,22 @@ namespace Core.ViewModels.Movie
 {
     public class MovieDetailsModel
     {
+        public string Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public string Description { get; set; }
+
+        public TimeSpan Duration { get; set; }
+
+        public int ReleaseYear { get; set; }
+
+        public string Actors { get; set; }
+
+        public string Director { get; set; }
+
+        public string Country { get; set; }
     }
 }
