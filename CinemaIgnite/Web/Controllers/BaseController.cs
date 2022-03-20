9697dded466c2fc5921ce219746adedd59e4a59f
Bozhikov99 <72099913+Bozhikov99@users.noreply.github.com>
2022-03-21 01:00:47 +0200

    Implemented logic for showing users' the unread notification count on the Layout via UnreadNotificationsViewComponent.cs

--- CinemaIgnite/Web/Controllers/BaseController.cs
+++ CinemaIgnite/Web/Controllers/BaseController.cs
@@ -4,5 +4,12 @@ namespace Web.Controllers
 {
     public class BaseController : Controller
     {
+        //public async Task<PartialViewResult> Notifications()
+        //{
+        //    string userId = userService.GetUserId();
+        //    int unread = await notificationService.GetUnreadCount(userId);
+        //    ViewBag.UnreadCount = unread;
+        //    return PartialView("_NotificationPartial");
+        //}
     }
 }
