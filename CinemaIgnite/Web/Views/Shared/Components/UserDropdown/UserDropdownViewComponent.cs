0f891bd2c250de90ca8b1afb9d6abf866ae31c27
Bozhikov99 <72099913+Bozhikov99@users.noreply.github.com>
2022-03-26 19:59:03 +0200

    Completed the layout for the app.

---
+++ CinemaIgnite/Web/Views/Shared/Components/UserDropdown/UserDropdownViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using Core.Services.Contracts;
+using Core.ViewModels.User;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Views.Shared.Components.UserDropdown
+{
+    public class UserDropdownViewComponent: ViewComponent
+    {
+        private readonly IUserService userService;
+
+        public UserDropdownViewComponent(IUserService userService)
+        {
+            this.userService = userService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            UserProfileModel model = await userService.GetUserProfile();
+
+            return View(model);
+        }
+    }
+}
