46781355f79ce4b47b4af15ff0e5914f036c29d5
Bozhikov99 <72099913+Bozhikov99@users.noreply.github.com>
2022-04-05 11:53:53 +0300

    Created InMemoryDbContext.cs so we could test with an in-memory database.

---
+++ CinemaIgnite/Test/InMemoryDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Infrastructure;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    public class InMemoryDbContext
+    {
+        private readonly SqliteConnection connection;
+        private readonly DbContextOptions<CinemaDbContext> options;
+
+        public InMemoryDbContext()
+        {
+            connection = new SqliteConnection("FileName=:memory:");
+            connection.Open();
+
+            options = new DbContextOptionsBuilder<CinemaDbContext>()
+                .UseSqlite(connection)
+                .Options;
+
+            using CinemaDbContext context = new CinemaDbContext(options);
+            context.Database.EnsureCreated();
+        }
+
+        public CinemaDbContext CreateContext() => new CinemaDbContext(options);
+
+        public async Task Dispose() => await connection.DisposeAsync();
+    }
+}
