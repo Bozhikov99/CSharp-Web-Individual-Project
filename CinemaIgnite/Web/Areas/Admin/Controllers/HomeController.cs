53cf59d2bced9989450da230fd7a7f53cb26b2e4
Bozhikov99 <72099913+Bozhikov99@users.noreply.github.com>
2022-04-10 16:14:36 +0300

    Added resources for the Admin layout and Home page

--- CinemaIgnite/Web/Areas/Admin/Controllers/HomeController.cs
+++ CinemaIgnite/Web/Areas/Admin/Controllers/HomeController.cs
@@ -1,11 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Localization;
 
 namespace Web.Areas.Admin.Controllers
 {
     public class HomeController : BaseController
     {
+        private readonly IHtmlLocalizer<HomeController> localizer;
+
+        public HomeController(IHtmlLocalizer<HomeController> localizer)
+        {
+            this.localizer = localizer;
+        }
+
         public IActionResult Index()
         {
+            var adminWelcome = localizer["AdminWelcome"];
+            var areaSummary = localizer["AreaSummary"];
+            var pageTitle = localizer["PageTitle"];
+            ViewData["AdminWelcome"] = adminWelcome;
+            ViewData["AreaSummary"] = areaSummary;
+            ViewData["PageTitle"] = pageTitle;
+
             return View();
         }
     }
